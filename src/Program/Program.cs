1659deacae99ca47ccb1aa8d96bfd9ec5bd640ad
Ignacio Valle <nacho.valle20@gmail.com>
2021-10-24 21:36:34 -0300

    Agrego Permisos

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -23,11 +23,38 @@ namespace ConsoleApplication
             train.StartEngines();
             Console.WriteLine("Hello World!"); */
 
-           AreaOfWork.AddAreaOfWork("carpinteria");
-           AreaOfWork.AddAreaOfWork("construccion");
-        
-           AreaOfWork.GetTextToPrint();
+            AreaOfWork.AddAreaOfWork("carpinteria");
+            AreaOfWork.AddAreaOfWork("construccion");
+            AreaOfWork.AddAreaOfWork("herreria");
+            AreaOfWork.AddAreaOfWork("quimica");
 
+            AreaOfWork.GetTextToPrint();
+
+            Company Compania1 = new Company("compania1");
+            Console.WriteLine("Ingrese Rubro: ");
+            string rubro=Console.ReadLine();
+            if (AreaOfWork.CheckAreaOfWork(Convert.ToInt16(rubro)))
+            {
+                Compania1.AddAreaOfWork(AreaOfWork.GetAreaOfWorkByIndex(Convert.ToInt16(rubro)));    
+            }
+            else
+            {
+                Console.WriteLine("No exite el rubro Ingresado");
+            }
+
+            Console.WriteLine($"{Compania1.AreaOfWork}");
+
+            IPermissions permiso1 = new PermissionHazardous("permiso1");
+            IPermissions permiso2 = new PermissionBase("permiso2");
+            IPermissions permiso3 = new PermissionBase("permiso3");
+
+            Compania1.AddPermission(permiso1);
+            Compania1.AddPermission(permiso2);
+            
+           // Compania1.GetPermissions();
+            Console.WriteLine(Compania1.SearchP(permiso1));
+            Console.WriteLine(Compania1.SearchP(permiso3));
+            
 
         }
     }
