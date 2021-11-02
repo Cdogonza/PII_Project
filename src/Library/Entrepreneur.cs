f8e12f2c2e9bd73e8d1466fc9b7fc754d4463a57
Bruno Olivera <brunos.olivera@correo.ucu.edu.uy>
2021-11-01 22:31:48 -0300

    Agrego XML y cambios a Entrepreneur

--- src/Library/Entrepreneur.cs
+++ src/Library/Entrepreneur.cs
@@ -2,21 +2,19 @@ using System.Collections.Generic;
 
 namespace ClassLibrary
 {
-    public class Entrepreneur : UserBase
-    {
         /// <summary>
         ///  Esta clase representa al emprendedor el cual hereda metodos y atributos de la clase UserBase
         /// </summary>
+    public class Entrepreneur : UserBase
+    {
+        /// <summary>
+        /// Constructor de la clase Entrepreneur
+        /// </summary>
+        /// <returns>Entrepreneur</returns>
         public Entrepreneur(string name,int phone) : base (name,phone)
         {
-            /// <summary>
-            /// Constructor de la clase Entrepreneur
-            /// </summary>
-            /// <returns>Entrepreneur</returns>
             
         }
-        
-       
     }
 
 }
