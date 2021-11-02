0a92bb721705f1d1bddbb7fff68a506b1fe629bc
Nacho <nacho.valle20@gmail.com>
2021-11-02 18:18:52 -0300

    agrego comentarios Xml en ConsolePrinter

--- src/Library/ConsolePrinter.cs
+++ src/Library/ConsolePrinter.cs
@@ -3,49 +3,102 @@ using System;
 namespace ClassLibrary
 {
 
-    // DIP
-    public class ConsolePrinter //: IPrinter
+    /// <summary>
+    /// 
+    /// </summary>
+    public class ConsolePrinter : IPrinter
     {
-        //public Company Company {get;set;}
-
+        /// <summary>
+        /// Este metodo se encarga de imprimir en pantalla lo que devuelve GetOffertsAvailability de OfferManager
+        /// </summary>
+        /// <param name="offermanager"></param>
         public void PrintOffertsAvailability(OfferManager offermanager)
         {
             Console.WriteLine(offermanager.GetOffertsAvailability());
         }
+
+        /// <summary>
+        ///  Este metodo se encarga de imprimir en pantalla lo que devuelve GetMyOffertsAvailability de OfferManager
+        /// </summary>
+        /// <param name="offermanager"></param>
+        /// <param name="company"></param>
         public void PrintMyOffertsAvailability(OfferManager offermanager, Company company)
         {
             Console.WriteLine(offermanager.GetMyOffertsAvailability(company));
         }
+
+        /// <summary>
+        ///  Este metodo se encarga de imprimir en pantalla lo que devuelve GetOfferByLocation de Search
+        /// </summary>
+        /// <param name="search"></param>
+        /// <param name="location"></param>
         public void PrintOffertsByLocation(Search search, string location)
         {
             Console.WriteLine(search.GetOfferByLocation(location));
         }
+        
+        /// <summary>
+        /// Este metodo se encarga de imprimir en pantalla lo que devuelve GetOfferByWord de Search
+        /// </summary>
+        /// <param name="search"></param>
+        /// <param name="word"></param>
         public void PrintOffertsByWord(Search search, string word)
         {
             Console.WriteLine(search.GetOfferByWord(word));
         }
+
+        /// <summary>
+        /// Este metodo se encarga de imprimir en pantalla lo que devuelve GetOfferByCategory de Search
+        /// </summary>
+        /// <param name="search"></param>
+        /// <param name="category"></param>
         public void PrintOffertsByCategory(Search search, string category)
         {
             Console.WriteLine(search.GetOfferByCategory(category));
         }
+        
+        /// <summary>
+        /// Este metodo se encarga de imprimir en pantalla lo que devuelve GetOfferByEntrepreneur de Search
+        /// </summary>
+        /// <param name="search"></param>
+        /// <param name="entrepreneur"></param>
         public void PrintOffertsByEntrepreneur(Search search, Entrepreneur entrepreneur)
         {
             Console.WriteLine(search.GetOfferByEntrepreneur(entrepreneur));
         }
+        
+        /// <summary>
+        /// Este metodo se encarga de imprimir en pantalla lo que devuelve GetOfferByCompany de Search
+        /// </summary>
+        /// <param name="search"></param>
+        /// <param name="company"></param>
         public void PrintOffertsByCompany(Search search, Company company)
         {
             Console.WriteLine(search.GetOfferByCompany(company));
         }
     
+        /// <summary>
+        /// Este metodo se encarga de imprimir en pantalla lo que devuelve GetTextToPrintPermission de Datamanager
+        /// </summary>
+        /// <param name="datamanager"></param>    
         public void PrintPermissions(DataManager datamanager)
         {
             Console.WriteLine(datamanager.GetTextToPrintPermission());
         }
+
+        /// <summary>
+        /// Este metodo se encarga de imprimir en pantalla lo que devuelve GetTextToPrintPermission de Datamanager
+        /// </summary>
+        /// <param name="datamanager"></param>
         public void PrintAreaOfWork(DataManager datamanager)
         {
             Console.WriteLine(datamanager.GetTextToPrintAreaOfWork());
         }
 
+        /// <summary>
+        /// Este metodo se encarga de imprimir en pantalla lo que devuelve GetTextToPrintMaterialType de Datamanager
+        /// </summary>
+        /// <param name="datamanager"></param>
         public void PrintMaterialType(DataManager datamanager)
         {
             Console.WriteLine(datamanager.GetTextToPrintMaterialType());
--- src/Library/Search.cs
+++ src/Library/Search.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 namespace ClassLibrary
 {
     /// <summary>
-    /// Esta clase filtra el catálogo según diferentes términos
+    /// Filtra el catálogo según diferentes términos
     /// </summary>
     public class Search
     {
@@ -12,14 +12,14 @@ namespace ClassLibrary
         /// </summary>
         private List<Offer> catalog = new List<Offer>();
         /// <summary>
-        /// Constructor - carga una copia del catalogo de ofertas a la lista
+        /// Carga una copia del catalogo de ofertas a la lista
         /// </summary>
         public Search()
         {
-            this.catalog = OfferManager.catalog;
+            this.catalog = Singleton<OfferManager>.Instance.catalog;
         }
         /// <summary>
-        /// Filtra el catálogo de ofertas según su ubicación
+        /// Filtra el catálogo de búsquedas según su ubicación
         /// </summary>
         /// <param name="location"></param>
         /// <returns>Retorna un string con una lista de ofertas</returns>
@@ -40,7 +40,7 @@ namespace ClassLibrary
             return data;
         }
         /// <summary>
-        /// Filtra el catálogo de ofertas según palabras clave
+        /// Filtra el catálogo de búsquedas según palabras clave
         /// </summary>
         /// <param name="word"></param>
         /// <returns>Retorna un string con una lista de ofertas</returns>
@@ -62,7 +62,7 @@ namespace ClassLibrary
 
         }
         /// <summary>
-        /// Filtra el catálogo de ofertas según su categoria
+        /// Filtra el catálogo de búsquedas según su categoria
         /// </summary>
         /// <param name="category"></param>
         /// <returns>Retorna un string con una lista de ofertas</returns>
@@ -83,7 +83,7 @@ namespace ClassLibrary
             return data;
         }
         /// <summary>
-        /// Filtra el catálogo de ofertas y agrupa las que compró un emprendedor
+        /// Filtra el catálogo de búsquedas que compró un emprendedor
         /// </summary>
         /// <param name="entrepreneur"></param>
         /// <returns>Retorna un string con una lista de ofertas</returns>
@@ -100,9 +100,8 @@ namespace ClassLibrary
             }
             return data;
         }
-
         /// <summary>
-        /// Filtra el catálogo de ofertas y agrupa las que publicó una empresa
+        /// Filtra el catálogo de búsquedas que publicó una empresa
         /// </summary>
         /// <param name="company"></param>
         /// <returns>Retorna un string con una lista de ofertas</returns>
@@ -116,34 +115,6 @@ namespace ClassLibrary
                 if(offer.Company == company)
                 {
                     data = data + $"ID: {offer.id} Name: {offer.Name} - Material: {offer.Material} - Cost: {offer.Cost}  Fecha y hora de publicacion {offer.PublicationDate} \n";
-                }
-            }
-            return data;
-        }
-
-        /// <summary>
-        /// Filtra el catálogo de ofertas y agrupa las que están disponibles 
-        /// y fueron publicadas por determinada compañia
-        /// </summary>
-        /// <param name="company"></param>
-        /// <returns>Retorna un string con una lista de ofertas</returns>
-        public string GetAvailableOffersByCompany(Company company)
-        {
-
-            string data = $"Las ofertas habilitadas para la compania son: \n";
-
-            foreach (Offer offer in catalog)
-            {
-                if(offer.Company == company)
-                {
-                    if(offer.Availability)
-                    {
-                        data = data + $"ID: {offer.id} Name: {offer.Name} - Material: {offer.Material} - Cost: {offer.Cost}  Fecha y hora de publicacion {offer.PublicationDate} \n";
-                    }
-                    else
-                    {
-                        data = "No tienes Ofertas habilitadas para mostrar";
-                    }
                 }
             }
             return data;
