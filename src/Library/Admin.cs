9e2be7e7c807b2d2539ca937ecf690da086df9cf
Ignacio Valle <nacho.valle20@gmail.com>
2021-11-03 21:34:49 -0300

    Clase admin

---
+++ src/Library/Admin.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+using System;
+namespace ClassLibrary
+{   
+    /// <summary>
+    /// Esta clase representa al administrador del Bot
+    /// </summary>
+    public class Admin
+    {
+        /// <summary>
+        /// Propiedad nombre del Administrador
+        /// </summary>
+        public string Name{get;set;}
+        
+        /// <summary>
+        /// Constructor de la clase Admin
+        /// </summary>
+        public Admin(string name)
+        {
+            if (String.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentNullException(name);
+            }
+            this.Name = name;
+        }
+        /// <summary>
+        /// Metodo que se utilizara para invitar a las Empresas al sistema.
+        /// </summary>
+        /// <param name="company"></param>
+        public void InviteCompany(string company)
+        {
+            
+        }
+    }
+}
