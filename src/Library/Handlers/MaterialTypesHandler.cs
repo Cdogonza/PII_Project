57ebb02c80fe2368ac681dcdc510f030d68548ee
Nacho <nacho.valle20@gmail.com>
2021-11-27 20:05:08 -0300

    agrego cancel en registrationhandler y materialtypes

--- src/Library/Handlers/MaterialTypesHandler.cs
+++ src/Library/Handlers/MaterialTypesHandler.cs
@@ -45,54 +45,65 @@ namespace ClassLibrary
             
             if(Singleton<TelegramUserData>.Instance.userdata[message.UserId].Count >= 1)
             {
-                if (Singleton<TelegramUserData>.Instance.userdata[message.UserId][0].ToLower().Trim().Contains("/materialtype") )
+                if(message.Text.ToLower().Equals("/cancel") )
                 {
-                    if(message.Text.ToLower().Equals("/listar") )
-                    {
                     //Singleton<TelegramUserData>.Instance.userdata[message.UserId].Add(message.Text);
-                    response = $"{Singleton<DataManager>.Instance.GetTextToPrintMaterialType()}";
-
+                    Singleton<TelegramUserData>.Instance.userdata.Remove(message.UserId);
+                    response = "Se cancela el proceso actual";
                     return true;
-                    }                   
-                    
-                    if(message.Text.ToLower().Equals("/agregar_tipodemateriales") )
+                }
+                else{    
+                    if (Singleton<TelegramUserData>.Instance.userdata[message.UserId][0].ToLower().Trim().Contains("/materialtype") )
                     {
-                        Console.WriteLine("Entre a agregar tipo de materiales");
-                        if (Singleton<DataManager>.Instance.GetCompany(message.UserId) != null )
+                        if(message.Text.ToLower().Equals("/listar") )
                         {
-                
-                            Console.WriteLine("matcheo");
-                            Singleton<TelegramUserData>.Instance.userdata[message.UserId].Add(message.Text);
-                            Console.WriteLine($"0 - {Singleton<TelegramUserData>.Instance.userdata[message.UserId][0]}");
-                            Console.WriteLine($"Count - {Singleton<TelegramUserData>.Instance.userdata[message.UserId].Count}");
-                            response = "Ingrese el nombre del Tipo de Material";
-                            return true;
+                        //Singleton<TelegramUserData>.Instance.userdata[message.UserId].Add(message.Text);
+                        response = $"{Singleton<DataManager>.Instance.GetTextToPrintMaterialType()}";
+
+                        return true;
+                        }                   
+                        
+                        if(message.Text.ToLower().Equals("/agregar_tipodemateriales") )
+                        {
+                            Console.WriteLine("Entre a agregar tipo de materiales");
+                            if (Singleton<DataManager>.Instance.GetCompany(message.UserId) != null )
+                            {
+                    
+                                Console.WriteLine("matcheo");
+                                Singleton<TelegramUserData>.Instance.userdata[message.UserId].Add(message.Text);
+                                Console.WriteLine($"0 - {Singleton<TelegramUserData>.Instance.userdata[message.UserId][0]}");
+                                Console.WriteLine($"Count - {Singleton<TelegramUserData>.Instance.userdata[message.UserId].Count}");
+                                response = "Ingrese el nombre del Tipo de Material";
+                                return true;
+                            }
+                            else
+                            {
+                                response = "No tiene privilegios suficientes para agregar materiales, Solo las empresas pueden agregar materiales ";
+                                return true;
+                            }
                         }
-                        else
+
+                        if (Singleton<TelegramUserData>.Instance.userdata[message.UserId][1].ToLower().Contains("/agregar_tipodemateriales"))
                         {
-                            response = "No tiene privilegios suficientes para agregar materiales, Solo las empresas pueden agregar materiales ";
+                            if(Singleton<TelegramUserData>.Instance.userdata[message.UserId].Count == 2)
+                            {
+                            Singleton<TelegramUserData>.Instance.userdata[message.UserId].Add(message.Text);
+                            response = "Ingrese la descripcion del Tipo de Material:";
                             return true;
+                            }
+                            if(Singleton<TelegramUserData>.Instance.userdata[message.UserId].Count == 3)
+                            {
+                                Singleton<TelegramUserData>.Instance.userdata[message.UserId].Add(message.Text);
+                                Singleton<DataManager>.Instance.AddMaterialType(Singleton<TelegramUserData>.Instance.userdata[message.UserId][2],Singleton<TelegramUserData>.Instance.userdata[message.UserId][3]);
+                                response = $"Se creo el Tipo de material con exito - {Singleton<TelegramUserData>.Instance.userdata[message.UserId][2]} - {Singleton<TelegramUserData>.Instance.userdata[message.UserId][3]} ";
+
+                                return true;
+                            }
                         }
                     }
 
-                    if (Singleton<TelegramUserData>.Instance.userdata[message.UserId][1].ToLower().Contains("/agregar_tipodemateriales"))
-                    {
-                        if(Singleton<TelegramUserData>.Instance.userdata[message.UserId].Count == 2)
-                        {
-                        Singleton<TelegramUserData>.Instance.userdata[message.UserId].Add(message.Text);
-                        response = "Ingrese la descripcion del Tipo de Material:";
-                        return true;
-                        }
-                        if(Singleton<TelegramUserData>.Instance.userdata[message.UserId].Count == 3)
-                        {
-                            Singleton<TelegramUserData>.Instance.userdata[message.UserId].Add(message.Text);
-                            Singleton<DataManager>.Instance.AddMaterialType(Singleton<TelegramUserData>.Instance.userdata[message.UserId][2],Singleton<TelegramUserData>.Instance.userdata[message.UserId][3]);
-                            response = $"Se creo el Tipo de material con exito - {Singleton<TelegramUserData>.Instance.userdata[message.UserId][2]} - {Singleton<TelegramUserData>.Instance.userdata[message.UserId][3]} ";
-                            
-                            return true;
-                        }
-                    }
                 }
+
             }
                 response = String.Empty ;
                 return false;         
