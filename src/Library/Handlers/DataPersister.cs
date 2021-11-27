74ca10c1b5ba364c74d5040969f7713e29b6618a
Cdogonza <gonzalo.ucu@correo.ucu.edu.uy>
2021-11-27 03:25:30 -0300

    Arreglando persistencia

--- src/Library/Handlers/DataPersister.cs
+++ src/Library/Handlers/DataPersister.cs
@@ -6,7 +6,7 @@ using System.Linq;
 namespace ClassLibrary
 {   
     
-    public  class DataPersister
+    public  class DataPersister 
     {
         public DataPersister()
         {
@@ -28,6 +28,27 @@ namespace ClassLibrary
             }else{
                 File.WriteAllText($"{@colection}.json",json);
             }
+            // {
+        //     // string result = "{\"Items\":[";
+
+        //     // foreach (var item in this.content.Items)
+        //     // {
+        //     //     result = result + item.ConvertToJson() + ",";
+        //     // }
+
+        //     // result = result.Remove(result.Length - 1);
+        //     // result = result + "]}";
+
+        //     // string temp = JsonSerializer.Serialize(this.content);
+        //     // return result;
+        //     JsonSerializerOptions options = new()
+        //     {
+        //         ReferenceHandler = MyReferenceHandler.Instance,
+        //         WriteIndented = true
+        //     };
+
+        //     return JsonSerializer.Serialize(this.content, options);            
+        // }
             
         }
         
